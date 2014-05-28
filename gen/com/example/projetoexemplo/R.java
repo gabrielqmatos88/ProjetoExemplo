5a18adde1b404b0d40a2c874cb083d440a33c400
Gabriel Q. Matos <gabriel.q.matos@gmail.com>
2014-05-28 00:43:43 -0300

    Alterações usando recursos

--- gen/com/example/projetoexemplo/R.java
+++ gen/com/example/projetoexemplo/R.java
@@ -1253,9 +1253,10 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
-        public static final int hintLogin=0x7f0a0011;
-        public static final int hintSenha=0x7f0a0012;
+        public static final int hintLogin=0x7f0a0012;
+        public static final int hintSenha=0x7f0a0013;
         public static final int login=0x7f0a0010;
+        public static final int senha=0x7f0a0011;
     }
     public static final class style {
         /** 
